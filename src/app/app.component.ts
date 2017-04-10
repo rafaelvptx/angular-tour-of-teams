1dad9e09b655b607957dd911b7b90f03bb96adb9
zacchello <canardmagret@gmail.com>
2017-04-10 13:29:39 +0200

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    styleUrls: ['./app.component.css'],
-    template: `
-        <h1>{{title}}</h1>
-        <nav>
-            <a routerLink="/leagues" routerLinkActive="active">Leagues</a>
-        </nav>
-        <router-outlet></router-outlet>
-    `
+  selector: 'my-app',
+  styleUrls: ['./app.component.css'],
+  template: `
+   <ul>
+        <li><a class="active" href="">Home</a></li>
+        <li><a href="">Journ&eacute;es</a></li>
+        <li><a href="">Equipes</a></li>
+         <li><a  href="">Retour</a></li>
+   
+     
+    </ul>
+    <div id="page"><router-outlet></router-outlet></div>`
 })
 
 export class AppComponent {
-    title = 'Coop';
+
 }
--- src/app/day.js
+++ src/app/day.js
@@ -1,5 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var Day = (function () {
     function Day() {
     }
